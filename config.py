7393d89e716e164c9f59165d9a2fc8fb4cda3325
umidjon-userbot <62179002+umidjon-userbot@users.noreply.github.com>
2021-04-28 09:33:06 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+HEROKU = False   # NOTE Make it false if you're not deploying on heroku.
+
+
+if not HEROKU:
+    API_ID = 1006088
+    API_HASH = "35f4b10cab604a1cb4088f8d83471129"
+    SUDO_CHAT_ID = -1001343491131
+    SUDOERS = [660086073,324672799]
+
+# don't make changes below this line
+ARQ_API = "https://thearq.tech"
---
+++ misc.py
@@ -0,0 +1,18 @@
+HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
+
+**/help** __Show This Message.__
+**/skip** __Skip The Current Playing Music.__
+**/play** __youtube/saavn/deezer Song_Name__
+**/telegram** __Play From Telegram Audio.__
+**/joinvc** __Join Voice Chat.__
+**/rejoinvc** __Rejoin Voice Chat.__
+**/leavevc** __Leave Voice Chat.__
+**/volume [1-200]** __Adjust Volume.__
+**/pause** __Pause Music.__
+**/resume** __Resume Music.__
+**/update** __Update & Restart.__"""
+
+START_TEXT = "__**Hi I'm Telegram Voice Chat Bot. *__"
+
+REPO_TEXT = "[Github](https://github.com/thehamkercat/Telegram_vc_bot)" \
+            + " | [Group](t.me/PatheticProgrammers)"
