91f5705285fdcf66872fe5b905a75cf8d5fd2934
umidjon-userbot <62179002+umidjon-userbot@users.noreply.github.com>
2021-04-30 12:52:45 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ HEROKU = False   # NOTE Make it false if you're not deploying on heroku.
 if not HEROKU:
     API_ID = 1006088
     API_HASH = "35f4b10cab604a1cb4088f8d83471129"
-    SUDO_CHAT_ID = -1001343491131
-    SUDOERS = [660086073,324672799]
+    SUDO_CHAT_ID = [-1001343491131,-1001162402454]
+    SUDOERS = [660086073,324672799,1202496132]
 
 # don't make changes below this line
 ARQ_API = "https://thearq.tech"
