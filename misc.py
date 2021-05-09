6b9d269c947104aadf267adb3d13bef6b5c62988
umidjon-userbot <62179002+umidjon-userbot@users.noreply.github.com>
2021-05-09 15:01:10 +0500

    Update misc.py

--- misc.py
+++ misc.py
@@ -12,7 +12,7 @@ HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
 **/resume** __Resume Music.__
 **/update** __Update & Restart.__"""
 
-START_TEXT = "__**Hi I'm Telegram Voice Chat Bot. *__"
+START_TEXT = "__**Salom, Men Telegram ovozli chatda musiqa qo'yib beruvchi Userbotman ! *__"
 
 REPO_TEXT = "[Github](https://github.com/thehamkercat/Telegram_vc_bot)" \
             + " | [Group](t.me/PatheticProgrammers)"
