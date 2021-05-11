3ec48a05ba0803adfcb1eb0f4a18f1a855c38e12
umidjon-userbot <62179002+umidjon-userbot@users.noreply.github.com>
2021-05-11 12:43:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ HEROKU = False   # NOTE Make it false if you're not deploying on heroku.
 if not HEROKU:
     API_ID = 1006088
     API_HASH = "35f4b10cab604a1cb4088f8d83471129"
-    SUDO_CHAT_ID = [-1001361154501, -1001343491131, 1]
+    SUDO_CHAT_ID = [-1001343491131, 1]
     SUDOERS = [660086073,324672799,1202496132]
 
 # don't make changes below this line
